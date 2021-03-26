dfa47892aede16e8bed416c55da6b69c21ee23b1
Zixuan Zhu <xxxape_zixuan@qq.com>
2021-03-26 16:39:05 +0800

    add user identity

---
+++ TE29_HeartHealth_GCardiac/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TE29_HeartHealth_GCardiac.Startup))]
+namespace TE29_HeartHealth_GCardiac
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
