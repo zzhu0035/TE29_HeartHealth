c7300de4eb15d2b638829dcfd135925af2fc83a4
Zixuan Zhu <xxxape_zixuan@qq.com>
2021-03-28 20:00:59 +0800

    user input page for exercise recommendation

---
+++ TE29_HeartHealth_GCardiac/Controllers/PreferenceController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TE29_HeartHealth_GCardiac.Controllers
+{
+    public class PreferenceController : Controller
+    {
+
+        // GET: Preference/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Preference/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ TE29_HeartHealth_GCardiac/Models/PreferenceViewModels.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TE29_HeartHealth_GCardiac.Models
+{
+    public class PreferenceViewModels
+    {
+        [Required]
+        [Display(Name = "If you have any previous medical conditions, select this option")]
+        public bool MedicalConditions { get; set; }
+
+        [Required]
+        [Display(Name = "Which type of sports do you prefer?")]
+        public String Sports { get; set; }
+
+        [Required]
+        [Display(Name = "For each exericse, how much minimum calories you wish to burn (per hour)?")]
+        public int MinCalory { get; set; }
+
+        [Required]
+        [Display(Name = "For each exericse, how much maximum calories you wish to burn (per hour)?")]
+        public int MaxCalory { get; set; }
+    }
+}
