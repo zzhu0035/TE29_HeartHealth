eeb8a09e89c751fab888700396eeada8df6db9e1
Leo <zzha0177@student.monash.edu>
2021-05-11 10:43:39 +0800

    improve
    validation ui

--- TE29_HeartHealth_GCardiac/Models/FamilyMember.cs
+++ TE29_HeartHealth_GCardiac/Models/FamilyMember.cs
@@ -11,16 +11,28 @@ namespace TE29_HeartHealth_GCardiac.Models
     {
         public int Id { get; set; }
 
+        [Required]
+        [Display(Name = "Name")]
+        [RegularExpression("[0-9a-zA-Z]{4,10}", ErrorMessage = "Please only enter the numbers & characters and the length should be 4-10 characters.")]
         public string Name { get; set; }
 
+        [Display(Name = "Age")]
+        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100.")]
         public int Age { get; set; }
 
+        [Display(Name = "Height(cm)")]
+        [Range(100, 250, ErrorMessage = "Please enter a number between 100 and 250.")]
         public int Height { get; set; }
 
+        [Display(Name = "Weight(kg)")]
+        [Range(30, 200, ErrorMessage = "Please enter a number between 30 and 200.")]
         public int Weight { get; set; }
 
+        [Display(Name = "HeartRate\n(times/min)")]
+        [Range(50, 150, ErrorMessage = "Please enter a number between 50 and 150.")]
         public int? HeartRate { get; set; }
 
+        [Display(Name = "Secret Reward")]
         public string SecretReward { get; set; }
 
         public DateTime Date { get; set; }
